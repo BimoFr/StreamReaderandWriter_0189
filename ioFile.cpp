f6e62ad242d3ac2b6d54ac2f291dc34a7e863961
BimoFr <bimofadhlurrahman@gmail.com>
2025-06-09 14:41:56 +0700

    menambahkan proses pembacaan file

--- ioFile.cpp
+++ ioFile.cpp
@@ -28,5 +28,9 @@ int main () {
 
     //membuat objek input file stream
     ifstream infile;
-    
+    //membuka file yang dituliskan
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+
 }
