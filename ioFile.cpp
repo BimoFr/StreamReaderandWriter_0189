19b3e040bcf911328526b71fd8c0fa7958639847
BimoFr <bimofadhlurrahman@gmail.com>
2025-06-09 14:28:04 +0700

    menambah proses penulisan ke file contohfile.txt menggunakan ofstream

--- ioFile.cpp
+++ ioFile.cpp
@@ -6,5 +6,10 @@ using namespace std;
 int main () {
     string baris;
 
-    //
+    //membuat objek output file stream  
+    ofstream outfile;
+    //membuuka file untu ditulisi
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 }
