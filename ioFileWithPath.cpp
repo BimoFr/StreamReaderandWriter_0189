aca57317df93458d6cb1c0db588b6ea1d165fc4b
BimoFr <bimofadhlurrahman@gmail.com>
2025-06-09 15:00:07 +0700

    menambahkan string namefile

---
+++ ioFileWithPath.cpp
@@ -0,0 +1,51 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+    string baris;
+
+    //membuat objek output file stream  
+    ofstream outfile;
+    //membuuka file untu ditulisi
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while(true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //membuat objek input file stream
+    ifstream infile;
+    //membuka file yang dituliskan
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ad maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+                //dan tampilkan disini
+                cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
+
+}
