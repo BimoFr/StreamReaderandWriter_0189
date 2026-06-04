775c7ddf1b84e23aa583186e9a8fa3f01ec837d3
BimoFr <bimofadhlurrahman@gmail.com>
2026-06-04 16:47:23 +0700

    nambah try-catch pake integer dan default exception

--- manualException.cpp
+++ manualException.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 int main()
 {
+    try
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+
+        cout << "Pernyataan tidak akan ditampilkan" << endl;
+    }
+    catch (int a)
+    {
+        cout << "Pengecualian akan di eksekusi" << endl;
+    }
+    catch (...)
+    {
+        cout << "Default pengecualian dieksekusi" << endl;
+    }
 
 }
